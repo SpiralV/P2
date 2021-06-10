065bb60a3e083c0cc89034246055adcbecaa22b3
SpiralV <MVelasco90@hotmail.com>
2021-06-10 11:47:51 -0700

    6/10 pre-lunch push

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+// import express
+const express = require('express')
+// import axios
+const axios = require('axios')
+// import ejsLayouts
+const ejsLayouts = require('express-ejs-layouts')
+require('dotenv').config()
+const port = 3000
+// define API key var
+const openkey = process.env.OPENKEY
+// declare an instance of app
+const app = express()
 
-const PORT = 3000
+app.set('view engine', 'ejs')
+app.use(express)
+app.use(ejsLayouts)
 
-app.listen(PORT, () => {
-    console.log(`yes this is ${PORT}`)
+app.get('/', (req, res) => {
+    axios.get(``)
+    .then((resReply) => {
+        res.send(resReply)
+    })
+    .catch(err => {console.log(err)})
+    })
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`yes this is ${port}`)
 })
