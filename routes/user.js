7a84428526656ad8b2786c21af03061de90ec566
SpiralV <MVelasco90@hotmail.com>
2021-06-27 19:06:03 -0700

    adding localStorage

--- routes/user.js
+++ routes/user.js
@@ -6,15 +6,27 @@ app.get('/', (req, res) => {
     res.render('user')
 })
 
-app.post('/', async(req, res) => {
-    let name = req.body.name
-  db.user.findOrCreate({
-    where: {
-      name: name
-    }
-  (res.redirect('user', { userName: name }))
-  }) 
-})
+// app.post('/', (req, res) => {
+//   let name = req.body.name
+//  db.user.findOrCreate({
+//   where: {name: name}})
+// res.render('user', { userName: name })
+//  })
+
+// app.post('/user_location', (req, res) => {
+//   let userIn = req.body.name
+//   res.send('hello')
+//   // res.render('user_location', { userName: userIn })
+// })
+// app.post('/', async(req, res) => {
+//     let name = req.body.name
+//   await db.user.findOrCreate({
+//     where: {
+//       name: name
+//     }.then(
+//   (res.redirect({ status: 200 }, 'user', { userName: name } ))
+//   )}).catch(err => console.log(err))
+// })
 
 // app.get('/', (req, res) => {
 //   let name = req.query.user
--- routes/user_location.js
+++ routes/user_location.js
@@ -5,5 +5,9 @@ const db = require('../models')
 app.get('/', (req, res) => {
     res.render('user_location')
 })
-
+app.post('/', (req, res) => {
+    let userIn = req.body.name
+    // res.send('hello')
+    res.render('user_location', { userName: userIn })
+  })
 module.exports = app
